332e248f38f92c2e1438f9fe380bdcdfdf1d4953
Andrew Longosz <andrzej.longosz@ez.no>
2019-01-28 16:15:14 +0100

    [DI] Created Symfony Extension for DoctrineSchemaBundle

---
+++ src/bundle/DependencyInjection/DoctrineSchemaExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\DoctrineSchemaBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class DoctrineSchemaExtension extends Extension
+{
+    /**
+     * Load Doctrine Schema Extension config.
+     *
+     * @param array $configs
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+        $container->addResource(new FileResource(__DIR__ . '/../Resources/config/services.yaml'));
+
+        $loader->load('api.yaml');
+        $container->addResource(new FileResource(__DIR__ . '/../Resources/config/api.yaml'));
+    }
+}
