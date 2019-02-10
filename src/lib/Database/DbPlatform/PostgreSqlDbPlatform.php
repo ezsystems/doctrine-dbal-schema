d621b21421a4b1b9beebcf084f82801271379fb9
Andrew Longosz <andrzej.longosz@ez.no>
2019-02-10 18:27:39 +0100

    EZP-30057: Created DbPlatform Factory of AbstractPlatform instances

---
+++ src/lib/Database/DbPlatform/PostgreSqlDbPlatform.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\DoctrineSchema\Database\DbPlatform;
+
+use Doctrine\DBAL\Event\SchemaDropTableEventArgs;
+use Doctrine\DBAL\Events;
+use Doctrine\DBAL\Platforms\PostgreSQL100Platform;
+use Doctrine\DBAL\Schema\Table;
+use InvalidArgumentException;
+
+class PostgreSqlDbPlatform extends PostgreSQL100Platform implements DbPlatform
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDriverName(): string
+    {
+        return 'pdo_pgsql';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreateSchemaSQL($schemaName)
+    {
+        return 'CREATE SCHEMA IF NOT EXISTS ' . $schemaName;
+    }
+
+    /**
+     * Returns the SQL snippet to drop an existing table.
+     *
+     * @param \Doctrine\DBAL\Schema\Table|string $table
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function getDropTableSQL($table)
+    {
+        $tableArg = $table;
+
+        if ($table instanceof Table) {
+            $table = $table->getQuotedName($this);
+        }
+
+        if (!is_string($table)) {
+            throw new InvalidArgumentException('getDropTableSQL() expects $table parameter to be string or \Doctrine\DBAL\Schema\Table.');
+        }
+
+        // note: unfortunately this logic was copied from parent
+        if ($this->_eventManager !== null && $this->_eventManager->hasListeners(Events::onSchemaDropTable)) {
+            $eventArgs = new SchemaDropTableEventArgs($tableArg, $this);
+            $this->_eventManager->dispatchEvent(Events::onSchemaDropTable, $eventArgs);
+
+            if ($eventArgs->isDefaultPrevented()) {
+                return $eventArgs->getSql();
+            }
+        }
+
+        return 'DROP TABLE IF EXISTS ' . $table . ' CASCADE';
+    }
+}
---
+++ src/lib/Database/DbPlatform/SqliteDbPlatform.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\DoctrineSchema\Database\DbPlatform;
+
+use Doctrine\DBAL\Platforms\SqlitePlatform;
+use Doctrine\DBAL\Schema\ForeignKeyConstraint;
+use Doctrine\DBAL\Schema\Table;
+
+class SqliteDbPlatform extends SqlitePlatform implements DbPlatform
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreateTableSQL(Table $table, $createFlags = null)
+    {
+        $createFlags = $createFlags ?? self::CREATE_INDEXES | self::CREATE_FOREIGNKEYS;
+
+        $hasCompositePK = $table->hasPrimaryKey() && count($table->getPrimaryKeyColumns()) > 1;
+
+        // drop autoincrement if table as composite key as this is not supported
+        if ($hasCompositePK) {
+            foreach ($table->getColumns() as $column) {
+                $column->setAutoincrement(false);
+            }
+        }
+
+        return parent::getCreateTableSQL($table, $createFlags);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDriverName(): string
+    {
+        return 'pdo_sqlite';
+    }
+
+    /**
+     * Override default behavior of Sqlite db platform to force generating foreign keys.
+     *
+     * @return bool
+     */
+    public function supportsForeignKeyConstraints(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Override default behavior of Sqlite db platform not to throw exception for unsupported operation of dropping FKs.
+     *
+     * {@inheritdoc}
+     */
+    public function getDropForeignKeySQL($foreignKey, $table)
+    {
+        // dropping FKs is not supported by Sqlite
+
+        return '-- ';
+    }
+
+    /**
+     * Override default behavior of Sqlite db platform not to throw exception for unsupported operation of creating FKs.
+     *
+     * {@inheritdoc}
+     */
+    public function getCreateForeignKeySQL(ForeignKeyConstraint $foreignKey, $table)
+    {
+        return '-- ';
+    }
+}
